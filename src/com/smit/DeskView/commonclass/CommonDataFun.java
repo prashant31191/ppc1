8f9b80b086683f608cdf3536ecae508de231a318
andrew <chenyzpower@gmail.com>
2011-08-09 10:08:02 +0800

    change rss icon and push ip

--- src/com/smit/DeskView/commonclass/CommonDataFun.java
+++ src/com/smit/DeskView/commonclass/CommonDataFun.java
@@ -2,5 +2,5 @@ package com.smit.DeskView.commonclass;
 
 public class CommonDataFun {
 	//public static String myServerAddr="http://localhost:8080/pring/";
-	public static String myServerAddr="http://192.168.0.187:8080/pring/";
+	public static String myServerAddr="http://192.168.0.190:8080/pushcmsserver/";
 }
